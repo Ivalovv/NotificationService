2010ed3f78b780e0b35d0b9937528e5d966c858b
Ivan Pavlov <mega.iwan2033@gmail.com>
2026-02-02 17:52:11 +0300

    дз6 fix

--- src/main/java/notification/dto/NotificationRequestDto.java
+++ src/main/java/notification/dto/NotificationRequestDto.java
@@ -1,12 +1,21 @@
 package notification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+@Schema(description = "Запрос на отправку уведомления по email")
 @Getter
 @Setter
 @NoArgsConstructor
 public class NotificationRequestDto {
 
+    @Schema(
+            description = "Email получателя"
+    )
     private String email;
+
+    @Schema(
+            description = "Текст уведомления"
+    )
     private String text;
 }
